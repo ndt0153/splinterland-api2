184b4cf79d0da6a339cb05a5f4e87a42fe0831fe
ndt0153 <ndt0153@gmail.com>
2021-11-22 23:31:26 +0700

    add timestamp

--- model/user.model.js
+++ model/user.model.js
@@ -1,22 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  dec: { type: Number },
-  erc: { type: Number },
-  rating: { type: Number },
-  power: { type: Number },
-  win: { type: Number },
-  lose: { type: Number },
-  draw: { type: Number },
-  total: { type: Number },
-  winrate: { type: Number },
-  quest: { type: String },
-  lastgame: { type: String },
-  lastdec: { type: String },
-  lastclaim: { type: String },
-  afk: { type: Number },
-  group: { type: String, unique: true },
-});
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    dec: { type: Number },
+    erc: { type: Number },
+    rating: { type: Number },
+    power: { type: Number },
+    win: { type: Number },
+    lose: { type: Number },
+    draw: { type: Number },
+    total: { type: Number },
+    winrate: { type: Number },
+    quest: { type: String },
+    lastgame: { type: String },
+    lastdec: { type: String },
+    lastclaim: { type: String },
+    afk: { type: Number },
+    group: { type: String, unique: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema, "User");
